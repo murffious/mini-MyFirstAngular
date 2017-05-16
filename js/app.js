ae2ee9329d977f74f9379c8ce99b10669026279e
Paul Murff <titan.murff@gmail.com>
2017-05-15 18:09:18 -0600

    done

---
+++ js/app.js
@@ -0,0 +1,2 @@
+angular.module('friendsList', []) 
+.controller('mainCtrl', function($scope){}) ;
---
+++ js/mainCtrl.js
@@ -0,0 +1,10 @@
+angular.module('friendsList')
+.controller('mainCtrl', function($scope) {
+
+$scope.friends = ["Brady", "Jim", "Greg", "Frank", "John"]
+
+
+
+
+});
+
